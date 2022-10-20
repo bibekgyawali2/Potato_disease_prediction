c42e9a4073a6b7849393116057017e692e727921
Bibek Gyawali <60395989+bibekgyawali2@users.noreply.github.com>
2022-10-20 15:27:37 +0545

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ app = FastAPI()
  #  "http://localhost",
   #  "http://localhost:3000",
 #]
-#app.add_middleware(
- #   CORSMiddleware,
-  #  allow_origins=origins,
-   # allow_credentials=True,
-    #allow_methods=["*"],
-    #allow_headers=["*"],
-#)
+app.add_middleware(
+   CORSMiddleware,
+   allow_origins=origins,
+   allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL = tf.keras.models.load_model("saved_models/1")
 
